ca67869ee9d143c9265d51b55561c107bcf99c0a
anupriya715 <124999600+anupriya715@users.noreply.github.com>
2023-02-14 18:38:10 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,14 @@
+class Swapnumbers
+{
+ public static void main(String args[])
+{
+	int a,b;
+	a=10;
+	b=30;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("a value is:"+a);
+	System.out.println("b value is:"+b);
+}
+}
