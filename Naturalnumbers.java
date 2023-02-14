9bf208f2db7455e244829f7c17ab22f9ed92b73a
anupriya715 <124999600+anupriya715@users.noreply.github.com>
2023-02-14 18:42:15 +0530

    Add files via upload

---
+++ Naturalnumbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Naturalnumbers
+{
+public static void main(String args[]){
+int i;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter any number");
+int n=sc.nextInt();
+
+for(i=1;i<=n;i++)
+{
+System.out.println(i);
+}
+}
+}
